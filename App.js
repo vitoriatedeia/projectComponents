1ec48eb359ca50c65a1f99fb06eda03e41bf0a65
Vitoria Tedeia <viih.tedeia@gmail.com>
2025-05-09 08:40:20 -0300

    projetctsComponents

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+// IMPORT STYLES
+import { styles } from "./src/styles/Styles";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <FirstComponent />
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const FirstComponent = () => {
+  return (
+    <View>
+      <Text>Meu primeiro componente</Text>
+    </View>
+  );
+};
